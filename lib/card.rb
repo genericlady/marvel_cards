413bac5bf148f0a7b6c552d2e12a19834a2996ca
yianna <yiannakokalas@gmail.com>
2017-12-30 12:04:41 -0500

    Remove constructor from Card
    - Card is now supported by the DB so we
      no longer need to define the constructor
      or accessor/mutators thanks to
      ActiveRecord

--- lib/card.rb
+++ lib/card.rb
@@ -1,11 +1,2 @@
-class Card
-  attr_accessor :number, :name, :type, :rarity
-
-  def initialize(card_hash={})
-    @number = card_hash[:number]
-    @name = card_hash[:name]
-    @type = card_hash[:type]
-    @rarity = card_hash[:rarity]
-  end
-
+class Card < ActiveRecord::Base
 end
