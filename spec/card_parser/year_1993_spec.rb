bbaa4aeab0b624c0c2ae4d08b8aa3aeba91d8344
yianna <yiannakokalas@gmail.com>
2017-12-30 12:02:36 -0500

    Add Tests and http recorder
    - Add Specs and record http transaction as a cassette to prevent
      unnecessary requests.

--- spec/card_parser/year_1993_spec.rb
+++ spec/card_parser/year_1993_spec.rb
@@ -1,7 +1,8 @@
 require 'spec_helper'
 
 describe CardParser::Year1993, :vcr do
-  let(:year_1993) { CardParser::Year1993.new }
+  let(:marvel_scraper) { MarvelScraper.new }
+  let(:year_1993) { CardParser::Year1993.new(marvel_scraper.card_list) }
 
   it "parse_commons will parse a string of commons into an array of cards." do
     commons = year_1993.commons
@@ -10,7 +11,7 @@ describe CardParser::Year1993, :vcr do
 
   it "parse_unsolved_mysteries will parse a string of unsolved mysteries." do
     unsolved_mysteries = year_1993.unsolved_mysteries
-    expect(unsolved_mysteries.first).to eq(Card)
+    expect(unsolved_mysteries.first.class).to eq(Card)
   end
 
 end
